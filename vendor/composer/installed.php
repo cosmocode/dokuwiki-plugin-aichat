ca3451493ff9ba1842fb5647bb2e9cb15f958c9e
Andreas Gohr <andi@splitbrain.org>
2023-06-08 16:30:21 +0200

    pin php to 8.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0337f47f2dd6cc31bdbfd81a2499e687ce633b47',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0337f47f2dd6cc31bdbfd81a2499e687ce633b47',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
