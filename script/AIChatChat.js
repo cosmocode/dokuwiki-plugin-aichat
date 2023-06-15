622aa5a8c9dcd98680628cfd3124b1128c168241
Andreas Gohr <andi@splitbrain.org>
2023-06-15 14:00:52 +0200

    added button to reset the conversation
    This is needed to start conversing on a new topic

--- script/AIChatChat.js
+++ script/AIChatChat.js
@@ -1,5 +1,6 @@
 class AIChatChat extends HTMLElement {
     #root = null;
+    #controls = null;
     #input = null;
     #output = null;
     #progress = null;
@@ -13,23 +14,23 @@ class AIChatChat extends HTMLElement {
             <div class="output"></div>
             <form>
                 <progress max="100" value="0"></progress>
-                <input type="text" autofocus />
+                <div class="controls">
+                    <button type="button" class="delete-history" title="Restart Conversation">
+                        <svg viewBox="0 0 24 24"><path d="M12,4C14.1,4 16.1,4.8 17.6,6.3C20.7,9.4 20.7,14.5 17.6,17.6C15.8,19.5 13.3,20.2 10.9,19.9L11.4,17.9C13.1,18.1 14.9,17.5 16.2,16.2C18.5,13.9 18.5,10.1 16.2,7.7C15.1,6.6 13.5,6 12,6V10.6L7,5.6L12,0.6V4M6.3,17.6C3.7,15 3.3,11 5.1,7.9L6.6,9.4C5.5,11.6 5.9,14.4 7.8,16.2C8.3,16.7 8.9,17.1 9.6,17.4L9,19.4C8,19 7.1,18.4 6.3,17.6Z" /></svg>
+                    </button>
+                    <input type="text" autofocus />
+                </div>
             </form>
         `;
         this.#root.appendChild(this.getStyle());
         this.#input = this.#root.querySelector('input');
         this.#output = this.#root.querySelector('.output');
         this.#progress = this.#root.querySelector('progress');
+        this.#controls = this.#root.querySelector('.controls');
         const form = this.#root.querySelector('form');
         form.addEventListener('submit', this.onSubmit.bind(this));
-    }
-
-    static get observedAttributes() {
-        return [
-            'placeholder', // placeholder for the input field
-            'hello', // initial fake message by the AI
-            'url', // URL to the AI server endpoint
-        ];
+        const restart = this.#root.querySelector('.delete-history');
+        restart.addEventListener('click', this.deleteHistory.bind(this));
     }
 
     /**
@@ -71,13 +72,29 @@ class AIChatChat extends HTMLElement {
                 clear: both;
                 margin-bottom: 1em;
             }
-            progress,
-            input {
+            .controls {
+                display: flex;
                 width: 100%;
             }
-            input {
+            .controls button {
+                padding: 0;
+                background: none;
+                border: none;
+                cursor: pointer;
+                display: flex;
+                width: 3em;
+            }
+            .controls button svg {
+                flex-grow: 1;
+                flex-shrink: 1;
+            }
+            .controls input {
+                flex-grow: 1;
                 padding: 0.25em;
                 font-size: 1.2em;  
+            }
+            progress{
+                width: 100%;
             }
             progress {
                 color: var(--color-link);
@@ -151,6 +168,13 @@ class AIChatChat extends HTMLElement {
         }
     }
 
+    deleteHistory() {
+        sessionStorage.removeItem('ai-chat-history');
+        this.#history = [];
+        this.#output.innerHTML = '';
+        this.connectedCallback(); // re-initialize
+    }
+
     /**
      * Submit the given question to the server
      *
@@ -189,7 +213,7 @@ class AIChatChat extends HTMLElement {
      * Hides the input field and shows the progress bar
      */
     startProgress() {
-        this.#input.style.display = 'none';
+        this.#controls.style.removeProperty('display');
         this.#progress.style.display = 'block';
         this.#progress.value = 0;
 
@@ -211,7 +235,7 @@ class AIChatChat extends HTMLElement {
             clearInterval(this.#progress._timer);
             this.#progress._timer = null;
         }
-        this.#input.style.display = 'initial';
+        this.#controls.style.display = 'flex';
         this.#progress.style.display = 'none';
     }
 
