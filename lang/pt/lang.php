1ec3b86c8ea255eb45fde3635cfb87613541b854
Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
2024-11-13 21:40:05 +0100

    translation update

---
+++ lang/pt/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
+ */
+$lang['title']                 = 'AI Chat';
+$lang['placeholder']           = 'Sua pergunta...';
+$lang['restricted']            = 'Desculpe, o chat AI não foi habilitado para você.';
+$lang['send-button']           = 'Enviar sua pergunta [Enter]';
+$lang['restart-button']        = 'Reiniciar a conversa';
+$lang['close-button']          = 'Fechar';
+$lang['fullscreen-button']     = 'Alternar tela cheia';
