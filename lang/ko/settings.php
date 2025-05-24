d6572921a202fb6797fdb9dff023e560b4dbbf88
SoiBKK <info@soibangkok.online>
2025-05-24 06:20:17 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author SoiBKK <info@soibangkok.online>
+ */
+$lang['openai_apikey']         = '🧠 <b>오픈AI</b> API key';
+$lang['gemini_apikey']         = '🧠 구글 <b>제미나이</b> API key';
+$lang['anthropic_apikey']      = '🧠 <b>앤트로픽</b> API key';
+$lang['mistral_apikey']        = '🧠 <b>미스트랄</b> API key';
+$lang['groq_apikey']           = '🧠 <b>그록</b> API key';
