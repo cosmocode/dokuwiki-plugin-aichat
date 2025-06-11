773012cf12c00971b494249c070969945d137830
Andreas Gohr <gohr@cosmocode.de>
2025-06-11 13:09:07 +0200

    first few tests to ensure model providers work

---
+++ _test/AbstractModelTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace dokuwiki\plugin\aichat\test;
+
+use DokuWikiTest;
+
+/**
+ * Base for Model provider tests
+ *
+ * @group plugin_aichat
+ * @group plugins
+ * @group internet
+ */
+abstract class AbstractModelTest extends DokuWikiTest
+{
+    /** @inheritdoc */
+    protected $pluginsEnabled = ['aichat'];
+
+    /** @var string The provider name, e.g. 'openai', 'reka', etc. */
+    protected string $provider;
+
+    /** @var string The environment variable name for the API key */
+    protected string $api_key_env;
+
+    /** @var string The chat model name, e.g. 'gpt-3.5-turbo', 'gpt-4', etc. */
+    protected string $chat_model;
+
+    /** @var string The embedding model name, e.g. 'text-embedding-3-small', etc. */
+    protected string $embedding_model;
+
+    /** @inheritdoc */
+    public function setUp(): void
+    {
+        parent::setUp();
+        global $conf;
+
+        $apikey = getenv($this->api_key_env);
+        if (!$apikey) {
+            $this->markTestSkipped('API key environment not set');
+        }
+
+        $providerName = ucfirst($this->provider);
+        $providerConf = strtolower($this->provider);
+
+        $conf['plugin']['aichat']['chatmodel'] = $providerName . ' ' . $this->chat_model;
+        $conf['plugin']['aichat']['rephrasemodel'] = $providerName . ' ' . $this->chat_model;
+        $conf['plugin']['aichat']['embeddingmodel'] = $providerName . ' ' . $this->embedding_model;
+        $conf['plugin']['aichat'][$providerConf . '_apikey'] = $apikey;
+    }
+
+    public function testChat()
+    {
+        global $conf;
+
+        $prompt = 'This is a test. Please reply with "Hello World"';
+
+        /** @var \helper_plugin_aichat $helper */
+        $helper = plugin_load('helper', 'aichat');
+        $model = $helper->getChatModel();
+        $reply = $model->getAnswer([
+            ['role' => 'user', 'content' => $prompt]
+        ]);
+
+        $this->assertStringContainsString('hello world', strtolower($reply));
+    }
+
+    public function testEmbedding()
+    {
+        $text = 'This is a test for embeddings.';
+
+        /** @var \helper_plugin_aichat $helper */
+        $helper = plugin_load('helper', 'aichat');
+        $model = $helper->getEmbeddingModel();
+        $embedding = $model->getEmbedding($text);
+        $this->assertIsArray($embedding);
+        $this->assertNotEmpty($embedding, 'Embedding should not be empty');
+        $this->assertIsFloat($embedding[0], 'Embedding should be an array of floats');
+    }
+}
